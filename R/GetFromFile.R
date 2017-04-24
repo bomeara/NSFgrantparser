7b3c2b479290a1ab56cbd26ce1351f000d47a86c
Brian O'Meara <omeara.brian@gmail.com>
2017-04-23 20:04:19 -0400

    updates to run fairly quickly

--- R/GetFromFile.R
+++ R/GetFromFile.R
@@ -1,6 +1,72 @@
 #pull from XML file saved from https://www.nsf.gov/awardsearch/download.jsp
 
-GetFromFile <- function(file) {
-  result <- XML::xmlToDataFrame(XML::xmlParse(file), stringsAsFactors = FALSE)
+GetFromFile<- function(file) {
+  result <- data.frame(t(unlist(XML::xmlToList(XML::xmlParse(file)))), stringsAsFactors=FALSE)
   return(result)
 }
+
+#table(unlist(lapply(aa, names))) #aa is list of all returns
+
+# GetAllDataFromYear <- function(year, verbose=TRUE) {
+#   tmp <- tempfile()
+#   curl::curl_download(paste0("https://www.nsf.gov/awardsearch/download?DownloadFileName=", year,"&All=true"), tmp)
+#   all.files <- unzip(tmp, list=FALSE)
+#   result <- GetFromFile(all.files[1])
+#   if(verbose) {
+#     print(paste("File 1 of", length(all.files),"for year", year))
+#   }
+#   #result <- result[,!is.na(names(result))]
+#   for (i in 2:length(all.files)) {
+#   #  print(dim(result))
+#     y <- GetFromFile(all.files[i])
+#   #  y <- y[,!is.na(names(y))]
+#   if(verbose & i%%1000==0) {
+#     print(paste("File",i,"of", length(all.files),"for year", year))
+#   }
+#     merge.names <- intersect(names(result), names(y))
+#     merge.names <- merge.names[!is.na(merge.names)]
+#     if (length(merge.names)>0) {
+#       result <- merge(result, y, by=merge.names, all=TRUE)
+#     }
+#   }
+#   return(result)
+# }
+
+GetAllDataFromYear <- function(year, verbose=TRUE) {
+  tmp <- tempfile()
+  curl::curl_download(paste0("https://www.nsf.gov/awardsearch/download?DownloadFileName=", year,"&All=true"), tmp)
+  if(verbose) {
+    print(paste("Starting year", year))
+  }
+  all.files <- unzip(tmp, list=FALSE)
+
+  result.list <- list(rep(NA, length(all.files)))
+  for (i in sequence(length(all.files))) {
+    if(verbose & i%%1000==0) {
+      print(paste("File",i,"of", length(all.files),"for year", year))
+    }
+    result.list[[i]] <- GetFromFile(all.files[i])
+  }
+  result <- plyr::rbind.fill(result.list)
+  return(result)
+}
+
+GetAllDataFromMultipleYears <- function(years=seq(from=2017, to=1959, by=-1), save.dir=getwd()) {
+  result <- GetAllDataFromYear(years[1])
+  write.csv(result, file=paste0(save.dir, "/NSFgrants_",years[1], ".csv"))
+  print(paste("There are", nrow(result),"grants recorded for", years[1]))
+  Sys.sleep(60)
+  if (length(years)>1) {
+    for (year.index in 2:length(years)) {
+      local.result <- GetAllDataFromYear(years[year.index])
+      write.csv(local.result, file=paste0(save.dir, "/NSFgrants_",years[year.index], ".csv"))
+      print(paste("There are", nrow(local.result),"grants recorded for", years[year.index]))
+      merge.names <- intersect(names(result), names(local.result))
+      result <- merge(result, local.result, by=merge.names, all=TRUE)
+    }
+  }
+  write.csv(result, file=paste0(save.dir, "/NSFgrants_AllSearchedYears.csv"))
+  return(result)
+}
+
+#look at plotting like http://www.axismaps.com/blog/2014/10/geography-of-jobs-animated-mapping-with-d3/
